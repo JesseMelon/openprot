95c6c599a19186d495208f085996b83493284a17
JesseMelon <jessechambers2000@icloud.com>
2026-06-01 10:51:20 -0400

    ast10x0 console backend UART flood fix

--- target/ast10x0/console_backend.rs
+++ target/ast10x0/console_backend.rs
@@ -21,7 +21,9 @@ static UART_LOCK: SpinLock<arch_arm_cortex_m::Arch, ()> = SpinLock::new(());
 
 #[unsafe(no_mangle)]
 pub fn console_backend_write_all(buf: &[u8]) -> Result<()> {
-    let _lock = UART_LOCK.lock(arch_arm_cortex_m::Arch);
+    let some(_lock) = UART_LOCK.lock(arch_arm_cortex_m::Arch) else {
+        return Ok(());
+    };
     // UART is configured by ROM/bootloader before firmware starts.
     let mut uart = unsafe { Usart::new_uninit(UART5_BASE) };
     uart.write_all(buf).map_err(|_| Error::DataLoss)
